f0349bb78528e2ebf46333e1f2e80556f430f8bd
teiniker <egon.teiniker@fh-joanneum.at>
2021-01-26 20:40:47 +0100

    Initial import

---
+++ actuators/servo/servo/servo.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+#include <Servo.h>
+
+const int SERVO_PWM_PIN = 9;
+
+Servo myservo;
+
+void setup(void)
+{
+  //connect pin 9 with the control line of the Servo
+  myservo.attach(SERVO_PWM_PIN); 
+} 
+
+void loop(void)
+{
+  myservo.write(90);  // 90°
+  delay(1000);
+
+  myservo.write(0);   // 0°
+  delay(1000);
+
+  myservo.write(90);  // 90°
+  delay(1000);
+
+  myservo.write(180); // 180°
+  delay(1000);
+}
