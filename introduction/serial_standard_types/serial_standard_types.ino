741eb773a116edec50c9c28dc5f4ca5391643556
teiniker <egon.teiniker@fh-joanneum.at>
2021-10-18 22:35:41 -0700

    Initial import.

---
+++ introduction/serial_standard_types/serial_standard_types.ino
@@ -0,0 +1,32 @@
+void setup() 
+{
+  Serial.begin(9600);                     
+  Serial.println("Arduino: standard types");  
+
+  // Signed integer types 
+  Serial.print("int8_t : "); 
+  Serial.println(sizeof(int8_t));                  
+  Serial.print("int16_t: "); 
+  Serial.println(sizeof(int16_t));                  
+  Serial.print("int32_t: "); 
+  Serial.println(sizeof(int32_t));                  
+  Serial.print("int64_t: "); 
+  Serial.println(sizeof(int64_t));                  
+
+  // Unsigned integer types 
+  Serial.print("uint8_t : "); 
+  Serial.println(sizeof(uint8_t));                  
+  Serial.print("uint16_t: "); 
+  Serial.println(sizeof(uint16_t));                  
+  Serial.print("uint32_t: "); 
+  Serial.println(sizeof(uint32_t));                  
+  Serial.print("uint64_t: "); 
+  Serial.println(sizeof(uint64_t));                  
+
+
+}
+
+void loop() 
+{
+  // do nothing
+}
