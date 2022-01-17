6c56fd065faf4a6b0f73c2332cb36255738fcc6b
teiniker <egon.teiniker@fh-joanneum.at>
2022-01-17 16:08:30 +0100

    Initial import.

---
+++ state-machine/sm-led/sm-led.ino
@@ -0,0 +1,107 @@
+const int LED_PIN = 2;
+const int SWITCH_ON_PIN = 9;
+const int SWITCH_OFF_PIN = 8;
+
+// Events 
+enum events
+{
+    SWITCH_ON,
+    SWITCH_OFF
+};
+enum events event = SWITCH_OFF;
+
+// State Machine States
+enum sm_states 
+{
+    OFF,
+    ON
+};
+
+enum sm_states state; 
+void sm_led(enum events event);
+void sm_led_off(enum events event);
+void sm_led_on(enum events event);
+
+// Activities
+int led_status = LOW;
+void led_on();
+void led_off();
+
+
+void setup() 
+{
+    pinMode(LED_PIN, OUTPUT);
+  	pinMode(SWITCH_ON_PIN, INPUT);
+  	pinMode(SWITCH_OFF_PIN, INPUT);
+}
+
+void loop() 
+{
+ 	// TODO: Cyclic Processing 
+  
+  	// IO Handling
+    digitalWrite(LED_PIN, led_status);
+	
+  	if(digitalRead(SWITCH_ON_PIN) == HIGH)
+      event = SWITCH_ON; 
+  	else if(digitalRead(SWITCH_OFF_PIN) == HIGH)
+      event = SWITCH_OFF;
+  	  
+	// Processing
+    sm_led(event);
+}
+
+// State Machine 
+void sm_led(enum events event)
+{
+    switch(state)
+    {
+        case OFF:
+            sm_led_off(event);
+            break;
+
+        case ON:
+            sm_led_on(event);
+            break;
+    }
+}
+
+// Handle Events in State: OFF
+void sm_led_off(enum events event)
+{
+    switch(event)
+    {
+        case SWITCH_ON:
+            led_on();
+            state = ON;
+        break;
+
+        case SWITCH_OFF:
+        break;
+    }
+}
+
+// Handle Events in State: ON
+void sm_led_on(enum events event)
+{
+    switch(event)
+    {
+        case SWITCH_ON:
+        break;
+
+        case SWITCH_OFF:
+            led_off();
+            state = OFF;
+        break;
+    }
+}
+
+void led_on()
+{
+  led_status = HIGH;
+}
+
+void led_off()
+{
+  led_status = LOW;
+}
