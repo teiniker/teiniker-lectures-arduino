e3a9d3a5d19bf7b20b253be16eec4aa57822cacb
teiniker <egon.teiniker@fh-joanneum.at>
2023-02-22 08:27:28 +0100

    Initial import.

---
+++ sensors-and-actuators/led-neopixel/neopixel-simple/neopixel-simple.ino
@@ -0,0 +1,51 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 2	 // input pin Neopixel is attached to
+
+#define NUMPIXELS 12 // number of neopixels in strip
+
+Adafruit_NeoPixel pixels = 
+  Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() 
+{  
+  pixels.begin();	// Initialize the NeoPixel library.
+}
+
+uint8_t data[NUMPIXELS][3] = 
+{
+  // R G B
+  {255,0,0}, 
+  {255,0,0},
+  {255,0,0},
+  {255,0,0},
+    
+  {0,255,0},
+  {0,255,0},
+  {0,255,0},
+  {0,255,0},
+    
+  {0,0,255},
+  {0,0,255},
+  {0,0,255},
+  {0,0,255}  
+};
+
+void loop() 
+{
+  pixels.setBrightness(64);
+  
+  for (int i=0; i < NUMPIXELS; i++) 
+  {
+    pixels.setPixelColor(i, pixels.Color(data[i][0], data[i][1], data[i][2]));
+    pixels.show();
+    delay(100);	  
+  }
+   
+  for (int i=0; i < NUMPIXELS; i++) 
+  {
+    pixels.setPixelColor(i, 0x000000);
+    pixels.show();
+    delay(100);	  
+  }
+}
