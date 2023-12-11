f08b16fd0536c1d2727c44ed4bfcb9df6f6756ac
teiniker <egon.teiniker@gmail.com>
2023-12-11 21:09:17 +0100

    Bugfix.

--- basics/io-ports/digital-output/digital-output.ino
+++ basics/io-ports/digital-output/digital-output.ino
@@ -9,17 +9,28 @@
  */
 
 //#define LED_PIN  13
-const int LED_PIN = 2;
+const int LED_PIN = 13;
 
 void setup() 
 {
-  pinMode(LED_PIN, OUTPUT);        
+  pinMode(LED_PIN, OUTPUT);   
+   Serial.begin(115200);     
 }
+
+void print(int8_t value, bool flag)
+{
+  Serial.println(value);
+  Serial.println(flag);
+}
+
 
 void loop() 
 {
   digitalWrite(LED_PIN, HIGH);      
   delay(1000);                       
+  
+  print(7.6, false);
+  delay(1000); 
   
   digitalWrite(LED_PIN, LOW);       
   delay(1000);                       
