225c87e404cc27c30e2a572ae65ce8f648d42226
teiniker <egon.teiniker@gmail.com>
2024-02-25 12:54:00 +0100

    Refactoring.

--- advanced/freertos/tasks/task-led/task-led.ino
+++ advanced/freertos/tasks/task-led/task-led.ino
@@ -1,5 +1,21 @@
-#define LED1_PIN 2
-#define LED2_PIN 4
+#include <Arduino_FreeRTOS.h>
+
+const uint8_t LED1_PIN = 2;
+const uint8_t LED2_PIN = 4;
+
+void setup() 
+{
+    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+
+    // Create the FreeRTOS tasks
+    xTaskCreate(led1_task, "LED1Task", 128, NULL, 2, NULL);
+    xTaskCreate(led2_task, "LED2Task", 128, NULL, 2, NULL);
+}
+
+void loop() 
+{
+}
 
 void led1_task(void *pvParameter) 
 {
@@ -23,20 +39,4 @@ void led2_task(void *pvParameter)
         digitalWrite(LED2_PIN, LOW);  // LED OFF
         vTaskDelay(250 / portTICK_PERIOD_MS);  // Delay for 250ms
     }
-}
-
-void setup() 
-{
-    pinMode(LED1_PIN, OUTPUT);
-    pinMode(LED2_PIN, OUTPUT);
-
-    // Create the FreeRTOS tasks
-    xTaskCreatePinnedToCore(led1_task, "LED1Task", 128, NULL, 2, NULL, 1);
-    xTaskCreatePinnedToCore(led2_task, "LED2Task", 128, NULL, 2, NULL, 1);
-}
-
-void loop() 
-{
-    // Just a delay to prevent the loop from running wild.
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
