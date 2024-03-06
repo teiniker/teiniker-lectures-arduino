3c0bc8db10caf221d4e2f5ee2c82b689210d7834
teiniker <egon.teiniker@gmail.com>
2024-03-06 10:38:58 +0100

    Refactoring.

--- advanced/freertos/tasks/task-led/task-led.ino
+++ advanced/freertos/tasks/task-led/task-led.ino
@@ -5,11 +5,8 @@ const uint8_t LED2_PIN = 6;
 
 void setup() 
 {
-    pinMode(LED1_PIN, OUTPUT);
-    pinMode(LED2_PIN, OUTPUT);
-
     // Create the FreeRTOS tasks
-    xTaskCreate(led1_task, "LED1Task", 128, NULL, 2, NULL);
+    xTaskCreate(led1_task, "LED1Task", 128, NULL, 1, NULL);
     xTaskCreate(led2_task, "LED2Task", 128, NULL, 2, NULL);
 }
 
@@ -19,24 +16,32 @@ void loop()
 
 void led1_task(void *pvParameter) 
 {
+    // setup
+    pinMode(LED1_PIN, OUTPUT);
+
+    // loop
     while(1) 
     {
         digitalWrite(LED1_PIN, HIGH);  
-        vTaskDelay(500 / portTICK_PERIOD_MS);  
+//        vTaskDelay(500 / portTICK_PERIOD_MS);  
 
         digitalWrite(LED1_PIN, LOW);  
-        vTaskDelay(500 / portTICK_PERIOD_MS);  
+//        vTaskDelay(500 / portTICK_PERIOD_MS);  
     }
 }
 
 void led2_task(void *pvParameter) 
 {
+    // setup
+    pinMode(LED2_PIN, OUTPUT);
+
+    // loop
     while(1) 
     {
         digitalWrite(LED2_PIN, HIGH);  
-        vTaskDelay(250 / portTICK_PERIOD_MS);  
+  //      vTaskDelay(250 / portTICK_PERIOD_MS);  
 
         digitalWrite(LED2_PIN, LOW);  
-        vTaskDelay(250 / portTICK_PERIOD_MS);  
+ //       vTaskDelay(250 / portTICK_PERIOD_MS);  
     }
 }
