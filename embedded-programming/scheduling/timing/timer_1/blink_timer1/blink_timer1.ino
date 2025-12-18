5fa96bdfe8daf8e24af6b560b86d91f7cbd06702
teiniker <egon.teiniker@gmail.com>
2025-12-18 10:46:59 +0100

    Added timer-1 example.

---
+++ embedded-programming/scheduling/timing/timer_1/blink_timer1/blink_timer1.ino
@@ -0,0 +1,30 @@
+#include <TimerOne.h>
+
+const int ledPin = 2;
+
+// volatile means that this variable can be changed in an interrupt
+volatile bool ledState = false;
+
+void ISR_blink_led() 
+{
+  ledState = !ledState;
+  digitalWrite(ledPin, ledState);
+}
+
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+
+  // Initialize Timer1 to trigger every 1,000,000 microseconds (1 second)
+  Timer1.initialize(1000000); 
+  
+  // Attach the ISR function to the timer
+  Timer1.attachInterrupt(ISR_blink_led); 
+}
+
+
+void loop() 
+{
+  // Nothing needed here, everything is handled in the ISR
+}
